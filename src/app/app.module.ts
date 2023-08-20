566064a29842716d1baea26cc4816ee8cfc894b2
guigomes91 <guilherme.gomes91@outlook.com>
2023-08-20 16:26:38 -0300

    HTML e CSS para componente NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { NavComponent } from './components/nav/nav.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     // Forms
     FormsModule,
