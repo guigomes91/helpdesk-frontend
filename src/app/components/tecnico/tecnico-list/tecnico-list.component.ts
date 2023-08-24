e8bb0ba2da6bc091ec6385fcb35f90527a1b0fbb
guigomes91 <guilherme.gomes91@outlook.com>
2023-08-24 19:41:43 -0300

    Ajustes da tabela de tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class TecnicoListComponent {
 
+  ELEMENT_DATA: Tecnico[] = [
+    { 
+      id: 1,
+      nome: 'Guilherme Gomes',
+      cpf: '123.456.789-10',
+      email: 'guilherme.gomes@outlook.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '24/08/2023'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
