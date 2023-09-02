075775634f175faacab5cfe666b11e6e6d716b9e
guigomes91 <guilherme.gomes91@outlook.com>
2023-09-02 09:45:04 -0300

    Autenticacao de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { Credenciais } from 'src/app/models/credenciais';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,8 @@ export class LoginComponent {
   }
 
   constructor(private toast: ToastrService,
-    private service: AuthService) {}
+    private service: AuthService,
+    private router: Router) {}
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
@@ -27,8 +29,11 @@ export class LoginComponent {
   }
 
   logar() {
-    this.service.authenticate(this.creds).subscribe(resposta => {
-      this.toast.info(resposta.headers.get('Authorization'))
+      this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7))
+      this.router.navigate(['']);
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidos');
     })
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Credenciais } from '../models/credenciais';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
 
   private readonly API = 'api/login';
 
@@ -16,5 +19,18 @@ export class AuthService {
       observe: 'response',
       responseType: 'text'
     });
+  }
+
+  successfulLogin(authToken: string) {
+    localStorage.setItem('token', authToken);
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if (token != null) {
+      return !this.jwtService.isTokenExpired(token);
+    }
+
+    return false;
   }
 }
